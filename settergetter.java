3a0f36952f728704601d9bcd7a7ac4dbf06ccacd
nnfuad <nnfuad01@gmail.com>
2024-07-03 14:53:58 +0600

    added first code

--- settergetter.java
+++ settergetter.java
@@ -1,3 +1,35 @@
-public class settergetter {
-    
+import java.util.*;
+
+public class settergetter{
+
+    public static class Student{
+
+        private String name;// purest form of encapsulation
+        private int roll;
+        
+        void setName(String a){
+            this.name = a;
+        }
+        void setRoll(int a ){
+            this.roll = a;
+        }
+        String getName(){
+            return name;
+        }
+        int getRoll(){
+            return roll;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student s1 = new Student();
+        System.out.println("Enter Student's name");
+        s1.setName(sc.nextLine());
+        System.out.println("Enter Student's roll");
+        s1.setRoll(sc.nextInt());
+        System.out.println("Student's name: "+s1.getName()+"\nStudent's Roll: "+s1.getRoll());
+        sc.close();
+    }
 }
+ 
